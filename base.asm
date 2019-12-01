be6905df3cf7922323fa08da6fcf4718ba00cb8f
viniciusth <38231257+viniciusth@users.noreply.github.com>
2019-12-01 16:35:11 -0300

    Terminando o trabalho 100%

--- base.asm
+++ base.asm
@@ -144,7 +144,159 @@ ret
 turn_left endp
 ;===================================
 ; INSTRUCOES:
+finalize proc:
+call end_program
+finalize endp
 
+pare proc:
+in al, 9
+cmp al, 1
+jne fim_pare
+mov al,0
+out 9,al
+fim_pare:
+call espera_robopronto
+pare endp
+;===================================
+; CONDICOES
+
+roboocupado proc
+in al,11
+test al, 00000010b
+jz false_roboocupado
+mov bx,1
+jmp fim_roboocupado
+false_roboocupado:
+mov bx,0
+fim_roboocupado:
+ret
+roboocupado endp
+
+roboparado proc
+in al,11
+test al, 00000010b
+jz true_roboparado
+in al, 9
+cmp al, 1
+jne true_roboparado
+mov bx,0
+jmp fim_roboparado
+true_roboparado:
+mov bx,1
+fim_roboocupado:
+ret
+roboparado endp
+
+robomovimentando proc
+call roboparado
+cmp bx,0
+je true_robomovimentando
+jmp fim_robomovimentando
+true_robomovimentando:
+mov bx,1
+fim_robomovimentando:
+ret
+robomovimentando endp
+
+frenterobobloqueada proc
+call examine
+in al, 10
+cmp al,0
+jne robo_bloqueado
+mov bx,0
+jmp fim_robobloqueado
+robo_bloqueado:
+mov bx,1
+fim_robobloqueado:
+ret
+frenterobobloqueada endp
+
+direitarobobloqueada proc
+call espera_robopronto
+call turn_right
+call espera_robopronto
+call frenterobobloqueada
+call turn_left
+call espera_robopronto
+ret
+direitarobobloqueada endp
+
+esquerdarobobloqueada proc
+call espera_robopronto
+call turn_left
+call espera_robopronto
+call frenterobobloqueada
+call turn_right
+call espera_robopronto
+ret
+esquerdarobobloqueada endp
+
+lampadaapagadaafrente proc
+call examine
+in al, 10
+cmp al, 8
+je true_lampadaapagadaafrente
+mov bx,0
+jmp fim_lampadaapagadaafrente
+true_lampadaapagadaafrente:
+mov bx,1
+fim_lampadaapagadaafrente:
+ret
+lampadaapagadaafrente endp
+
+
+lampadaacesaafrente proc
+call examine
+in al, 10
+cmp al, 7
+je true_lampadaacesaafrente
+mov bx,0
+jmp fim_lampadaacesaafrente
+true_lampadaacesaafrente:
+mov bx,1
+fim_lampadaacesaafrente:
+ret
+lampadaacesaafrente endp
+
+lampadaacesaadireita proc
+call espera_robopronto
+call turn_right
+call espera_robopronto
+call lampadaacesaafrente
+call turn_left
+call espera_robopronto
+ret
+lampadaacesaadireita endp
+
+lampadaacesaaesquerda proc
+call espera_robopronto
+call turn_left
+call espera_robopronto
+call lampadaacesaafrente
+call turn_right
+call espera_robopronto
+ret
+lampadaacesaaesquerda endp
+
+lampadaapagadaadireita proc
+call espera_robopronto
+call turn_right
+call espera_robopronto
+call lampadaapagadaafrente
+call turn_left
+call espera_robopronto
+ret
+lampadaapagadaadireita endp
+
+lampadaapagadaaesquerda proc
+call espera_robopronto
+call turn_left
+call espera_robopronto
+call lampadaapagadaafrente
+call turn_right
+call espera_robopronto
+ret
+lampadaapagadaaesquerda endp
 
 ;===================================
 end_program proc
